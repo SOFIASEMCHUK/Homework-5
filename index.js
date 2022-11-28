a8e49e1e6dd0d0f85a058091fbe247157fb0e163
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-11-28 22:49:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const firstName = prompt('What are your name?');
+const lastName = prompt('What ate your lastName?');
+const age = prompt('How old are you?')
+
+alert(`My name is ${firstName} ${lastName}, I have ${age} year.`);
+
+//54321
+const number1 = 5;
+const number2 = 4;
+const number3 = 3;
+const number4 = 2;
+const number5 = 1;
+
+alert(`${firstName} ${lastName} має ${number1} ${number2} ${number3} ${number4} ${number5} підписників на Ютубі`)
