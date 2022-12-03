9e46b068f94345de0c1ea617f489386d1c6be5b4
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-12-03 18:05:40 +0200

    add js file

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ const age = prompt('How old are you?')
 
 alert(`My name is ${firstName} ${lastName}, I have ${age} year.`);
 
-//54321
-const number1 = 5;
-const number2 = 4;
-const number3 = 3;
-const number4 = 2;
-const number5 = 1;
-
-alert(`${firstName} ${lastName} має ${number1} ${number2} ${number3} ${number4} ${number5} підписників на Ютубі`)
+const number = '54321';
+alert(number[0] + ' ' + number[1] + ' ' + number[2] + ' ' + number[3] + ' ' + number[4]);
